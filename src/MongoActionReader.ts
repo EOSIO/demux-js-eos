d23cb5d2eab90dd11dec33e2510350941ce17ca8
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2018-11-15 12:42:26 -0500

    Add better error handling to MongoActionReader

--- src/MongoActionReader.ts
+++ src/MongoActionReader.ts
@@ -3,6 +3,12 @@ import { Db, MongoClient } from "mongodb"
 
 import { MongoBlock } from "./MongoBlock"
 
+function wait(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 /**
  * Implementation of an ActionReader that reads blocks from a mongodb instance.
  */
@@ -24,32 +30,54 @@ export class MongoActionReader extends AbstractActionReader {
     this.mongodb = await mongoInstance.db(this.dbName)
   }
 
-  public async getHeadBlockNumber(): Promise<number> {
+  public async getHeadBlockNumber(numRetries: number = 120, waitTimeMs: number = 250): Promise<number> {
     this.throwIfNotInitialized()
 
-    const [blockInfo] = await this.mongodb!.collection("block_states")
-      .find({})
-      .limit(1)
-      .sort({ $natural: -1 })
-      .toArray()
+    let numTries = 1
 
-    if (this.onlyIrreversible) {
-      return blockInfo.block_header_state.dpos_irreversible_blocknum
+    while (numTries < numRetries) {
+      try {
+        const [blockInfo] = await this.mongodb!.collection("block_states")
+          .find({})
+          .limit(1)
+          .sort({ $natural: -1 })
+          .toArray()
+
+        if (this.onlyIrreversible) {
+          return blockInfo.block_header_state.dpos_irreversible_blocknum
+        }
+
+        return blockInfo.block_header_state.block_num
+      } catch (err) {
+        console.info("error getting head block number, retrying...")
+      }
+      numTries += 1
+      await wait(waitTimeMs)
     }
-
-    return blockInfo.block_header_state.block_num
+    throw Error("Retrieving head block number failed!")
   }
 
-  public async getBlock(blockNumber: number): Promise<MongoBlock> {
+  public async getBlock(blockNumber: number, numRetries: number = 120, waitTimeMs: number = 250): Promise<MongoBlock> {
     this.throwIfNotInitialized()
 
-    // Will not handle scenario of a fork since it only grabs first block
-    const [rawBlock] = await this.mongodb!.collection("blocks")
-      .find({ block_num: blockNumber })
-      .toArray()
+    let numTries = 1
 
-    const block = new MongoBlock(rawBlock)
-    return block
+    while (numTries < numRetries) {
+      try {
+        // Will not handle scenario of a fork since it only grabs first block
+        const [rawBlock] = await this.mongodb!.collection("blocks")
+          .find({ block_num: blockNumber })
+          .toArray()
+
+        const block = new MongoBlock(rawBlock)
+        return block
+      } catch (err) {
+        console.error("error retrieving block, retrying...")
+      }
+      numTries += 1
+      await wait(waitTimeMs)
+    }
+    throw Error("Retrieving block failed!")
   }
 
   private throwIfNotInitialized() {
