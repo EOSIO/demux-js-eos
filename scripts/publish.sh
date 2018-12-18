5cc98fcbd751b42f0473cee7b1d7396e55ee5ad4
Julien Heller <julien.heller@block.one>
2018-12-18 10:16:40 -0500

    Implement publish to master on tag, if tag matches current version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,23 +7,32 @@ setup_git() {
   git config --global user.email "devops@block.one"
   git config --global user.name "blockone-devops"
   git config --global push.default matching
-  
+
   # Get the credentials from a file
   git config credential.helper "store --file=.git/credentials"
-  
+
   # This associates the API Key with the account
   echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
 }
 
-make_version() {
-  # Make sure that the workspace is clean
-  # It could be "dirty" if
-  # 1. package-lock.json is not aligned with package.json
-  # 2. npm install is run
-  git checkout -- .
-  
-  # Echo the status to the log so that we can see it is OK
-  git status 
+test_master() {
+  if ! [ "$TRAVIS_BRANCH" = "automate-publish" -a -n "$TRAVIS_TAG" ]; then
+  echo "not master or tag!"
+    return 1
+  fi
+  if ! echo "$TRAVIS_TAG" | grep "^v[0-9]\.[0-9]\.[0-9]" >/dev/null; then
+    echo "tag not a version!"
+    return 1
+  fi
+  if ! [ "$TRAVIS_TAG" = "$(npm run version --silent)" ]; then
+    echo "tag not current version!"
+    return 1
+  fi
+  return 0
+}
+
+publish_edge() {
+  echo "Appending short commit hash to version"
 
   # Run the deploy build and increment the package versions
   current_commit="$(git rev-parse --short HEAD)";
@@ -31,6 +40,20 @@ make_version() {
   npm version prerelease -preid "${current_commit}" -no-git-tag-version
 
   git commit -a -m "Updating version [skip ci]"
+
+  echo "Publish edge to NPM"
+
+  cp .npmrc.template $HOME/.npmrc
+
+  npm publish --tag edge
+}
+
+publish_latest() {
+  echo "Publish new release to NPM"
+
+  cp .npmrc.template $HOME/.npmrc
+
+  npm publish
 }
 
 echo "Running on branch ${TRAVIS_BRANCH}":
@@ -38,12 +61,17 @@ echo "Running on branch ${TRAVIS_BRANCH}":
 echo "Setting up git"
 setup_git
 
-echo "Creating new version"
+# Make sure that the workspace is clean
+# It could be "dirty" if
+# 1. package-lock.json is not aligned with package.json
+# 2. npm install is run
+git checkout -- .
 
-make_version
+# Echo the status to the log so that we can see it is OK
+git status
 
-echo "Publish to NPM"
-
-cp .npmrc.template $HOME/.npmrc 
-
-npm publish --tag edge
+if test_master; then
+  publish_latest
+else
+  publish_edge
+fi
