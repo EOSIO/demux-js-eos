ed9f28e1b30d55ed9c286b1f73f2329151e8b162
Mike Creque <mike.creque@block.one>
2019-03-20 11:13:51 -0400

    linting

--- src/state-history/StateHistoryPostgresBlock.test.ts
+++ src/state-history/StateHistoryPostgresBlock.test.ts
@@ -61,7 +61,7 @@ describe('StateHistoryPostgresBlock', () => {
   it('handles blockNumber as string', async () => {
     stateHistoryPostgresBlock = new StateHistoryPostgresBlock(
       {
-        block_index: "4",
+        block_index: '4',
         block_id: 'qwerty1234',
         previous: 'qwerty1233',
         timestamp: new Date('2018-08-12').toString(),
@@ -80,6 +80,6 @@ describe('StateHistoryPostgresBlock', () => {
     )
     await stateHistoryPostgresBlock.parseActions()
 
-    expect(stateHistoryPostgresBlock.blockInfo.blockNumber).toEqual(4);
+    expect(stateHistoryPostgresBlock.blockInfo.blockNumber).toEqual(4)
   })
 })
