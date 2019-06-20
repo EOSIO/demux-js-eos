56f4d57dfcfb12b6296ed483952a9e2ef962158d
Julien Heller <julien.heller@block.one>
2019-06-20 11:06:38 -0400

    TestActionHandler

--- src/v1.7/testHelpers/TestActionHandler.ts
+++ src/v1.7/testHelpers/TestActionHandler.ts
@@ -1,22 +1,15 @@
 import { MassiveEosActionHandler } from '../handlers/massiveeos'
 import { HandlerVersion } from 'demux'
-import { MigrationSequence } from 'demux-postgres'
 
 export class TestActionHandler extends MassiveEosActionHandler {
-  constructor(
-    protected handlerVersions: HandlerVersion[],
-    protected massiveInstance: any,
-    protected dbSchema: string = 'public',
-    protected migrationSequences: MigrationSequence[] = [],
-  ) {
-    super(handlerVersions, massiveInstance, dbSchema, migrationSequences)
-  }
   public _matchActionType(candidateType: string, subscribedType: string, payload: any): boolean {
     return this.matchActionType(candidateType, subscribedType, payload)
   }
+
   public _validateHandlerVersions(handlerVersions: HandlerVersion[]) {
     return this.validateHandlerVersions(handlerVersions)
   }
+
   public _validateActionType(actionType: string) {
     return this.validateActionType(actionType)
   }
